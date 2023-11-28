4853f4d9d6de9b14ed0b4789bc02b82f8ea68b11
Joao Brugnago <joao.psb20@gmail.com>
2023-11-27 21:24:28 -0300

    anima-numeros-refatorado

--- modules/anima-numeros.js
+++ modules/anima-numeros.js
@@ -1,3 +1,56 @@
+export default class AnimaNumeros {
+  constructor(numeros, observerTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numeros);
+    this.observerClass = observerClass;
+    this.observerTarget = document.querySelector(observerTarget);
+    this.handleMutation = this.handleMutation.bind(this);
+  }
+
+  //-- Rcebe um elemento do Dom com número em seu texto. Incrementa a aprtir de zero até o número final
+  static incrementarNumero(numero) {
+    const total = +numero.innerText;
+    const incremento = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start = start + incremento;
+      if (start < total) {
+        numero.innerText = start;
+      } else {
+        clearInterval(timer);
+        numero.innerText = total;
+      }
+    }, 25 * Math.random());
+  }
+
+  //-- A função de callback está como this.constructor porque a função incrementarNumero é static, ou seja, está disponível apenas para o construtor (classe).
+  //-- Ela foi criada como static pois ela não utiliza nenhum elemento que faça parte do escopo da classe, ou seja, não utiliza o this.
+  animaNumeros() {
+    this.numeros.forEach((numero) =>
+      this.constructor.incrementarNumero(numero)
+    );
+  }
+
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect();
+      this.animaNumeros();
+    }
+  }
+
+  addMutationObserver() {
+    this.observer = new MutationObserver(this.handleMutation);
+    this.observer.observe(this.observerTarget, { attributes: true });
+  }
+
+  init() {
+    if (this.numeros.length && this.observerTarget) {
+      this.addMutationObserver();
+    }
+    return this;
+  }
+}
+
+/* Código antigo
 export default function initAnimaNumeros() {
   function animaNumeros() {
     const numeros = document.querySelectorAll("[data-numero]");
@@ -30,3 +83,4 @@ export default function initAnimaNumeros() {
   const observer = new MutationObserver(handleMutation);
   observer.observe(observerTarget, { attributes: true });
 }
+*/
--- modules/fetch-animais.js
+++ modules/fetch-animais.js
@@ -1,17 +1,22 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
   async function fetchAnimais(url) {
-    const responseAnimais = await fetch(url);
-    const jsonAnimais = await responseAnimais.json();
-    const numerosGrid = document.querySelector(".numeros-grid");
-
-    jsonAnimais.forEach((animal) => {
-      const divAnimal = createAnimal(animal);
-      numerosGrid.appendChild(divAnimal);
-    });
-    initAnimaNumeros();
+    try {
+      const responseAnimais = await fetch(url);
+      const jsonAnimais = await responseAnimais.json();
+      const numerosGrid = document.querySelector(".numeros-grid");
+  
+      jsonAnimais.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      const animaNumeros = new AnimaNumeros("[data-numero]", ".numeros", 'ativo')
+      animaNumeros.init();
+    } catch (erro) {
+    console.log(erro)
   }
+}
 
   function createAnimal(animal) {
     const div = document.createElement("div");
