31c877084978ab12ce57cae30b23600d9d30bf7e
Joao Brugnago <joao.psb20@gmail.com>
2023-11-29 22:27:30 -0300

    fetch animais refatorado

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
+import fetchAnimais from "./modules/fetch-animais.js";
 import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
@@ -28,8 +28,9 @@ tooltip.init();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initAnimacaoScroll();
+
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 
 /* Código antes da refatoração
 import initScrollSuave from "./modules/scroll-suave.js";
